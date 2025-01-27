1f839314a6e579964d93cef77dd8c27b11b4d431
aosaf-e-c <aosaf@connect.hku.hk>
2025-01-27 16:11:56 +0600

    Add layout, assets + improve homepage

--- ecom-shop/src/app/(shop)/index.tsx
+++ ecom-shop/src/app/(shop)/index.tsx
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { PRODUCTS } from '../../../assets/products';
+import { ProductListItem } from "../components/product-list-item";
 
 const index = () => {
   return (
     <View>
-      <Text>Hello World</Text>
+      <FlatList data={PRODUCTS}
+        renderItem={({item}) => <ProductListItem product={item} />}
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5}}
+      />
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContent : {
+    paddingBottom: 20,
+  },
+  flatListColumn : {
+    justifyContent: 'space-between',
+  }
+});
